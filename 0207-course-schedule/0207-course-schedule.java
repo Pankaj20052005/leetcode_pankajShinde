493d30bb059f6edf6f67a6d19dcb1bc314d1e338
Pankaj Shinde <153424976+Pankaj20052005@users.noreply.github.com>
2026-03-31 20:30:18 +0530

    Time: 4 ms (92.82%), Space: 47 MB (71.24%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i<numCourses; i++){
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] pre : prerequisites){
+            graph.get(pre[1]).add(pre[0]);
+        }
+
+        int[] visited = new int[numCourses];
+
+        for(int i = 0; i<numCourses; i++){
+            if(!dfs(graph, visited, i)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static boolean dfs(List<List<Integer>> graph, int[] visited, int node){
+        if(visited[node] == 1) return false;
+        if(visited[node] == 2) return true;
+
+        visited[node] = 1;
+
+        for(int nei : graph.get(node)){
+            if (!dfs(graph, visited, nei)){
+                return false;
+            }
+        }
+        visited[node] = 2;
+        return true;
+    }
+}
