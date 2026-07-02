1e3e4dedb950162b0b7b4e2421d770c498c741ad
Pankaj Shinde <153424976+Pankaj20052005@users.noreply.github.com>
2026-07-02 16:52:44 +0530

    Time: 4 ms (82.22%), Space: 46.4 MB (33.58%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        char[] sortS = s.toCharArray();
+        char[] sortT = t.toCharArray();
+
+        Arrays.sort(sortS);
+        Arrays.sort(sortT);
+        
+        return Arrays.equals(sortS, sortT);
+    }    
+}
