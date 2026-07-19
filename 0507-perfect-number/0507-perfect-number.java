00be7be1239030d60367fcbbd0cc1546b9f2fb57
Pankaj Shinde <153424976+Pankaj20052005@users.noreply.github.com>
2026-07-19 14:57:40 +0530

    Time: 2119 ms (10.8%), Space: 42.2 MB (26.31%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,23 +1,12 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        List <Integer> list = new ArrayList<>();
-        for( int i =1; i< num; i++){
-            if(num%i == 0){
-                list.add(i);
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
             }
         }
 
-        int sum = 0;
-        for(int factor: list){
-            sum += factor;
-        }
-
-        if(sum == num){
-            return true;
-        }
-
-        else{
-            return false;
-        }
+        return sum == num;
     }
 }
