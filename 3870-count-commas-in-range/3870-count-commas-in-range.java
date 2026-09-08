629730bb9c29e55f92bf93e377d7f6bfaf73a350
Pankaj Shinde <153424976+Pankaj20052005@users.noreply.github.com>
2026-09-08 10:45:09 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (72.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,14 +1,18 @@
 class Solution {
     public int countCommas(int n) {
-        int ans = 0;
-        if(n < 1000){
-            ans = 0;
-        }
+        // int ans = 0;
+        // if(n < 1000){
+        //     ans = 0;
+        // }
 
-        else if(n >= 1000 && n <= 100000){
-            ans = n - 999;
-        }
+        // else if(n >= 1000 && n <= 100000){
+        //     ans = n - 999;
+        // }
         
-        return ans;
+        // return ans;
+
+        if(n >= 1000) return n - 999;
+
+        else return 0;
     }
 }
